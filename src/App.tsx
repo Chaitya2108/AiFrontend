7e4d914938127f5de9896f266c499736c333fcbd
Chaitya Jodhavat <chaityajodhavat@gmail.com>
2024-05-09 21:33:38 -0700

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,6 @@ import { fetchData } from './fetchData';
 
 const App = () => {
   const [dataSource, setDataSource] = useState<any[]>([])
-  // const fetchAndAssign = async() => {
-  //   const data = await fetchData()
-  //   setDataSource(data)
-  // };
-  // const fetchAndAssign = useCallback(async () => {
-  //   const data = await fetchData()
-  //   setDataSource(data)
-  // }, [])
-
-  // useEffect(() => {
-  //   fetchAndAssign();
-  // }, [fetchAndAssign])
-
-  // const buttonClicked = () => {
-  //   fetchAndAssign()
-  // }
   const fetchAndAssign = async () => {
     await new Promise(resolve => setTimeout(resolve, 200));
     const data = await fetchData()
@@ -42,17 +26,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <Button type='primary' onClick={buttonClicked} style={{marginBottom:"2vh",marginTop:"2vh"}}>Fetch Data</Button>
-        {/* {
-          (dataSource != undefined) && (
-            <Table dataSource={dataSource} columns={columns} pagination={false} /> 
-          )
-        } */}
-        <Table dataSource={dataSource} columns={columns} pagination={false} /> 
-        
-        
-          
-        
-            
+        <Table dataSource={dataSource} columns={columns} pagination={false} />         
       </header>
     </div>
   );
